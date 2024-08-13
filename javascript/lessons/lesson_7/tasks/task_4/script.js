aa5ee36bcc3139351e06bbd5d27ccb1666d1a1b7
Eric Alekseyev <80809146+m1neil@users.noreply.github.com>
2024-08-13 16:12:50 +0300

    Add files via upload

--- javascript/lessons/lesson_7/tasks/task_4/script.js
+++ javascript/lessons/lesson_7/tasks/task_4/script.js
@@ -1,4 +1,24 @@
 'use strict'
-//  Створити функцію, яка для 3 заданих чисел знаходить одночасно декілька результатів:
-// кількість парних, кількість додатних, кількість більших за 100.
 
+let quantityPositiveNumbers = 0,
+	quantityEvenNumbers = 0,
+	quantityNumberMoveHundred = 0
+
+for (let indexNumber = 0; indexNumber < 3; indexNumber++) {
+	searchEvenPositiveMoreHundredNumbers(parseFloat(prompt('Ведіть число')))
+}
+
+const output = document.querySelector('.page__container')
+output.insertAdjacentHTML('beforeend', `
+	<ol class="menu-list">
+		<li class="menu-list__item">Кількість парних чисел: ${quantityEvenNumbers}</li>
+		<li class="menu-list__item">Кількість додатних чисел: ${quantityPositiveNumbers}</li>
+		<li class="menu-list__item">Кількість чисел більше за 100: ${quantityNumberMoveHundred}</li>
+	</ol>
+`)
+
+function searchEvenPositiveMoreHundredNumbers(number) {
+	if (number % 2 === 0) quantityEvenNumbers++
+	if (number > 0) quantityPositiveNumbers++
+	if (number > 100) quantityNumberMoveHundred++
+}
